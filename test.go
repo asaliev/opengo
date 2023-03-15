9bdfc6ee1bf17efd6753bedff95fcf2696eb4447
Asaliev Timur <asaliev@cydicom.com>
2023-03-15 11:12:47 +0200

    Read .env vars

--- test.go
+++ test.go
@@ -2,7 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
+
+func getEnvVar(key string) string {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+
+	return os.Getenv(key)
+}
 
 func main() {
 	fmt.Printf("Hello world")
