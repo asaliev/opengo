319ef5fa4391ac42e0438420914636eed297b1f3
Asaliev Timur <asaliev@cydicom.com>
2023-03-15 11:17:19 +0200

    Add openai library to test chatgpt

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/asaliev/opengo
 
 go 1.20
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/sashabaranov/go-openai v1.5.1 // indirect
+)
--- test.go
+++ test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 func getEnvVar(key string) string {
@@ -19,5 +21,25 @@ func getEnvVar(key string) string {
 }
 
 func main() {
-	fmt.Printf("Hello world")
+	token := getEnvVar("OPENAI_TOKEN")
+	client := openai.NewClient(token)
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello!",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
 }
