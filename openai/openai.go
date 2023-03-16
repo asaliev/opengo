17821b55bcbac5d18a3aaf9e6b7802f5822cb35b
asaliev <timur.asaliev@gmail.com>
2023-03-16 16:36:10 +0200

    add message context for chatgpt

--- openai/openai.go
+++ openai/openai.go
@@ -8,34 +8,56 @@ import (
 )
 
 type openaiProvider struct {
-	client *openai.Client
+	client   *openai.Client
+	messages []openai.ChatCompletionMessage
 }
+
+// How many context messages to send to ChatGPT
+const contextNumberOfMessages int = 5
 
 func NewOpenaiProvider(apiKeyName string) *openaiProvider {
 	config := config.NewConfigProvider()
 	client := openai.NewClient(config.ReadString(apiKeyName))
 	return &openaiProvider{
 		client: client,
+		messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleSystem,
+				Content: "You are a helpful assistant.",
+			},
+		},
 	}
 }
 
 func (p *openaiProvider) Ask(question string) (string, error) {
+	p.messages = append(p.messages, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleUser,
+		Content: question,
+	})
+
+	length := len(p.messages)
+	cut := contextNumberOfMessages
+	if length < contextNumberOfMessages {
+		cut = length
+	}
+	p.messages = p.messages[length-cut:]
+
 	resp, err := p.client.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
-			Model: openai.GPT3Dot5Turbo,
-			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleUser,
-					Content: question,
-				},
-			},
+			Model:    openai.GPT3Dot5Turbo,
+			Messages: p.messages,
 		},
 	)
 
 	if err != nil {
 		return "", err
 	}
+
+	p.messages = append(p.messages, openai.ChatCompletionMessage{
+		Role:    openai.ChatMessageRoleAssistant,
+		Content: resp.Choices[0].Message.Content,
+	})
 
 	return resp.Choices[0].Message.Content, nil
 }
