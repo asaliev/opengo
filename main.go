70c27999c8a35101412b3add117833385f62e237
asaliev <timur.asaliev@gmail.com>
2023-03-17 11:53:20 +0200

    refactor main

--- main.go
+++ main.go
@@ -2,99 +2,33 @@ package main
 
 import (
 	"bufio"
-	"errors"
 	"flag"
-	"fmt"
 	"os"
-	"time"
 
+	"github.com/asaliev/opengo/command"
 	"github.com/asaliev/opengo/openai"
-	"github.com/briandowns/spinner"
 )
 
-const (
-	apiKeyName     string = "OPENAI_TOKEN"
-	exitCommand    string = "exit"
-	promptMessage  string = "Query (exit to quit): "
-	goodbyeMessage string = "Goodbye..."
-)
+const apiKeyName string = "OPENAI_TOKEN"
 
 var queryPtr = flag.String("q", "", "Query sent to OpenAI")
 
 func main() {
 	flag.Parse()
+
+	var provider command.Command = nil
+	switch isFlagSet("q") {
+	case true:
+		scanner := bufio.NewScanner(os.Stdin)
+		provider = command.NewInteractiveCommand(scanner)
+
+	default:
+		provider = command.NewSimpleCommand(queryPtr)
+	}
+
 	openai := openai.NewOpenaiProvider(apiKeyName)
-	s := spinner.New(spinner.CharSets[9], 100*time.Millisecond)
-
-	// Get user input
-	for {
-		query := ""
-		if isFlagSet("q") {
-			queryFromArgs, err := getUserQueryFromArgs()
-			if err != nil {
-				panic(err.Error())
-			}
-			query = queryFromArgs
-		} else {
-			fmt.Println("Query (`exit` to quit):")
-			scanner := bufio.NewScanner(os.Stdin)
-			query = getUserInput(scanner)
-			if isExitCommand(query) {
-				fmt.Println(goodbyeMessage)
-				break
-			}
-		}
-
-		// Show the spinner only in interactive mode
-		if !isFlagSet("q") {
-			s.Start()
-		}
-
-		// Contact OpenAI
-		response, err := openai.Ask(query)
-		if err != nil {
-			if isFlagSet("q") {
-				panic(err.Error())
-			} else {
-				s.Stop()
-				fmt.Printf("ChatGPT error: %s", err.Error())
-				continue
-			}
-		}
-
-		// Hide the spinner only in interactive mode
-		if !isFlagSet("q") {
-			s.Stop()
-		}
-
-		fmt.Println(response)
-
-		// Exit when in non-interactive mode
-		if isFlagSet("q") {
-			break
-		}
-	}
-}
-
-func isExitCommand(input string) bool {
-	return input == exitCommand
-}
-
-func getUserQueryFromArgs() (string, error) {
-	if *queryPtr == "" {
-		return "", errors.New("empty -q parameter")
-	}
-	return *queryPtr, nil
-}
-
-func getUserInput(input *bufio.Scanner) string {
-	for input.Scan() {
-		if input.Text() != "" {
-			break
-		}
-	}
-
-	return input.Text()
+	service := command.NewCommandService(provider, *openai)
+	service.Execute()
 }
 
 func isFlagSet(name string) bool {
